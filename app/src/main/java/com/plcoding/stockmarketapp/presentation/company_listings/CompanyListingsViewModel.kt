24401ee4d39d9aaa2e3a90abf9f108b0e5cf3ac5
Igals <espf50@gmail.com>
2022-04-23 17:27:32 +0300

    Chapter 3 End - Staring UI

---
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsViewModel.kt
@@ -0,0 +1,74 @@
+package com.plcoding.stockmarketapp.presentation.company_listings
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.stockmarketapp.domain.repo.StockRepository
+import com.plcoding.stockmarketapp.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import java.lang.Error
+import javax.inject.Inject
+
+private const val TAG = "CompanyListingsVM"
+
+@HiltViewModel
+class CompanyListingsViewModel @Inject constructor(
+    private val repository: StockRepository
+) : ViewModel() {
+
+    var state by mutableStateOf(CompanyListingsState())
+
+    private var searchJob: Job? = null
+
+    fun onEvent(event: CompanyListingsEvent) {
+        when (event) {
+            is CompanyListingsEvent.Refresh -> {
+                getCompanyListing(fetchFromRemote = true)
+            }
+            is CompanyListingsEvent.OnSearchQueryChange -> {
+                state = state.copy(searchQuery = event.query)
+                searchJob?.cancel()
+                searchJob = viewModelScope.launch {
+                    delay(500)
+                    getCompanyListing()
+                }
+            }
+        }
+    }
+
+    private fun getCompanyListing(
+        query: String = state.searchQuery.lowercase(),
+        fetchFromRemote: Boolean = false
+    ) {
+        viewModelScope.launch {
+            repository
+                .getCompanyListings(fetchFromRemote, query)
+                .collect { result ->
+                    when (result) {
+                        is Resource.Loading -> {
+                            state = state.copy(isLoading = result.isLoading)
+                        }
+                        is Resource.Success -> {
+                            result.data?.let { listings ->
+                                state = state.copy(
+                                    companies = listings
+                                )
+                            }
+                        }
+                        is Resource.Error -> {
+                            Log.d(TAG, "getCompanyListingError: ${result.message}")
+                        }
+
+                    }
+                }
+        }
+
+    }
+}
